dec49d9b9ab4e704f8750dbb123e8abb80549962
Muhammad Kadafi Azis <muhammad.azis001@binus.ac.id>
2024-06-09 13:35:27 +0700

    firebase laravel notif

--- resources/views/InfoAntrian.blade.php
+++ resources/views/InfoAntrian.blade.php
@@ -117,7 +117,7 @@
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
     </style>
-
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
